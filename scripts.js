e759fc76642c4289734b7267dc1d4ef83a0103da
jastakpawel <innyjastak@gmail.com>
2017-06-04 18:41:37 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var buttonElements = document.getElementsByClassName('button');
+var numberOfElements = buttonElements.length;
+
+for (var buttonElements = 0; buttonElements<numberOfElements; buttonElements++) {
+	alert(buttonElements.innerText);
+}
