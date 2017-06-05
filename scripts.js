c2c60e3428f8e08ffccb81a9e62c8e1527c02b13
Pawel Jastak <innyjastak@gmail.com>
2017-06-05 10:57:55 +0200

    script change

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 var buttonElements = document.getElementsByClassName('button');
 var numberOfElements = buttonElements.length;
 
-for (var buttonElements = 0; buttonElements<numberOfElements; buttonElements++) {
-	alert(buttonElements.innerText);
+for (var i = 0; i<numberOfElements; i++) {
+	alert(buttonElements[0].innerText);
+	alert(buttonElements[1].innerText);
+	alert(buttonElements[2].innerText);
+	alert(buttonElements[3].innerText);
+	break;
 }
