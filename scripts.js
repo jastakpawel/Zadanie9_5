5511d39750a940deaaa9cbc10f7b26874c128703
Pawel Jastak <innyjastak@gmail.com>
2017-06-05 13:33:56 +0200

    alert one line

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 var buttonElements = document.getElementsByClassName('button');
 var numberOfElements = buttonElements.length;
 
-var siblings = buttonElements.nextElementSibling;
-
 for (var i = 0; i<numberOfElements; i++) {
 	alert(buttonElements[i].innerText);
 }
